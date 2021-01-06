b747dc5b485a663c58673be4abe79dabb518531e
ilicina1 <ilhan.licina@outlook.com>
2021-01-06 13:11:58 +0100

    responsive design

---
+++ src/RentDiv.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./RentDiv.css";
+import { Row, Col } from "antd";
+
+class RentDiv extends React.Component {
+    render() {
+      return (
+        <div className="rentDiv">
+          
+        </div>
+      );
+    }
+  }
+  
+  export default RentDiv;
+  
--- src/index.js
+++ src/index.js
@@ -1,30 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Layout, Menu } from 'antd';
-import { UserOutlined, LoginOutlined, PhoneOutlined } from '@ant-design/icons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Layout, Menu } from "antd";
+import { UserOutlined, LoginOutlined, PhoneOutlined } from "@ant-design/icons";
 import HeroText from "./HeroText";
-
+import { Row, Col } from "antd";
 
 const { Header, Content, Footer } = Layout;
 
 ReactDOM.render(
   <Layout className="layout">
     <Header>
-      <div className="logo"><a href="/"><h1>Logo<span>Go</span></h1></a></div>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1"><UserOutlined />My bookings</Menu.Item>
-        <Menu.Item key="2"><LoginOutlined />SignIn/SignUp</Menu.Item>
-        <Menu.Item key="3">Language</Menu.Item>
-        <Menu.Item key="4"><PhoneOutlined />Call Us</Menu.Item>
-      </Menu>
+      <Row gutter={[16, 24]}>
+        <Col span={12}>
+          <div className="logo">
+            <a href="/">
+              <h1>
+                Logo<span>Go</span>
+              </h1>
+            </a>
+          </div>
+        </Col>
+        <Col span={12}>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">
+              <UserOutlined />
+              My bookings
+            </Menu.Item>
+            <Menu.Item key="2">
+              <LoginOutlined />
+              SignIn/SignUp
+            </Menu.Item>
+            <Menu.Item key="3">Language</Menu.Item>
+            <Menu.Item key="4">
+              <PhoneOutlined />
+              Call Us
+            </Menu.Item>
+          </Menu>
+        </Col>
+      </Row>
     </Header>
     <Content>
       <div className="site-layout-content">
-      <HeroText/>
+        <HeroText />
       </div>
     </Content>
   </Layout>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
