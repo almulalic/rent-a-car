7d24c7fd4121f1064dc7125fe6d22109bc49f9a8
ilicina1 <ilhan.licina@outlook.com>
2021-01-10 16:00:08 +0100

    search section

--- src/pages/public/Order/components/SearchCar/SearchCar.js
+++ src/pages/public/Order/components/SearchCar/SearchCar.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./SearchCar.css";
 import { Row, Col, Button } from "antd";
 import { FilterWrap } from "./components/FilterWrap";
+import { ShowCar } from "./components/ShowCar";
 
 export const SearchCar = () => {
     return (
       <div className="searchCar">
         <FilterWrap />
-        <p>sss</p>
+        <ShowCar />
       </div>
     );
 };
